a2c57c4b7634f3e5026771df7828d907802c1d81
Dasha <d.s.grishina98@gmail.com>
2024-04-14 18:21:31 +0300

    Добавление библиотеки, удаление лишних строк кода

--- path.py
+++ path.py
@@ -1,8 +1,5 @@
 import os
 
-# путь к текущей папке
 PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-print(PROJECT_ROOT_PATH)
-# путь к папке с файлами
 OUTPOOT_PATH = os.path.join(PROJECT_ROOT_PATH, "output")
-print(OUTPOOT_PATH)
+
